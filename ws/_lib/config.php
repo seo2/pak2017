ac1acfcc3eed4aa344df60f447cbdafb1dac8b82
Seo2 <seo2@seo2.cl>
2017-06-28 20:05:18 -0400

    Paso a WP

--- ws/_lib/config.php
+++ ws/_lib/config.php
@@ -1,6 +1,6 @@
 <?php
-	define("DBHOST", "localhost");
-	define("DBUSER", "dbadmin");
-	define("DBPASS", "parauco2012");
-	define("DBNAME", "pak2017");
+	define("DBHOST", "internal-db.s219687.gridserver.com");
+	define("DBUSER", "db219687");
+	define("DBPASS", "4#LyMm}-3tm");
+	define("DBNAME", "db219687_pak");
 ?>
