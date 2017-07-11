9f8fd3ac16ef20127e16003304b95a530673e2b7
Seo2 <seo2@seo2.cl>
2017-07-10 21:47:05 -0400

    Función que permite cerrar el menú al clickear en cualquiera parte fuera de él.

--- assets/js/pak.js
+++ assets/js/pak.js
@@ -834,6 +834,8 @@ $(document).mouseup(function(e)
     // if the target of the click isn't the container nor a descendant of the container
     if (!container.is(e.target) && container.has(e.target).length === 0) 
     {
-        $('.navbar-toggle').click()
+	    if($('#navbar').hasClass('in')){
+        	$('.navbar-toggle').click();
+	    }
     }
 });
