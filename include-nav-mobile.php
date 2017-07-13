24b07354f7b6df6611017035119e5cc051dc7c83
fchristiansen <felipe@seo2.cl>
2017-07-13 18:03:59 -0400

    agregadas funciones WP a los href del menu mobile

--- include-nav-mobile.php
+++ include-nav-mobile.php
@@ -2,7 +2,7 @@
     <!--   <a class="navmenu-brand" href="#">Project name</a> -->
 
         <div class="logo text-center">
-                <a href="index.php"> parque arauco</a>
+                <a href="<?php bloginfo('url'); ?>"> parque arauco</a>
         </div> <!-- brand -->
 
     <div class="rrss visible-xs visible-sm center-block text-center">
@@ -11,7 +11,7 @@
         <li><a href="https://www.instagram.com/parquearauco/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
         <li><a href="https://twitter.com/parquearauco" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
        <!--  <li><a href="https://es.pinterest.com/parquearauco/" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li> -->
-       <li><a href="horarios.php"><i class="fa fa-clock-o" aria-hidden="true"></i></a></li>
+       <li><a href="<?php bloginfo('url'); ?>/horarios"><i class="fa fa-clock-o" aria-hidden="true"></i></a></li>
       </ul>
      </div><!-- RRSS -->
 
@@ -25,20 +25,21 @@
             <div class="container width">
                 <div class="row">
                   <ul class="nav navbar-nav lista_menu">
-                      <li class="active"><a href="index.php">Home</a></li>
-                      <li><a id="btn_distrito" href="distrito-de-lujo.php">distrito de lujo</a></li>
-                      <li><a id="btn_boulevard" href="boulevard.php#boulevard">boulevard</a></li>
-                      <li><a id="btn_piso" href="piso-diseno.php">piso diseño</a></li>
-                      <li><a id="btn_entretencion" href="novedades.php#entretencion">entretención</a></li>
-                      <li><a id="btn_novedades" href="novedades.php#novedades">novedades</a></li>
-                      <li><a id="btn_tiendas" href="tiendas.php#tiendas">tiendas</a></li>
+                      <li class="active"><a href="<?php bloginfo('url'); ?>">Home</a></li>
+                     <li><a id="btn_distrito"  href="<?php bloginfo('url'); ?>/distrito-de-lujo">distrito de lujo</a></li>
+                     <li><a id="btn_boulevard" href="<?php bloginfo('url'); ?>/boulevard#boulevard">boulevard</a></li>
+                     <li><a id="btn_piso" href="<?php bloginfo('url'); ?>/piso-diseno">piso diseño</a></li>
+                     <li><a id="btn_entretencion href="<?php bloginfo('url'); ?>/novedades#entretencion">entretención</a></li>
+                     <li><a id="btn_novedades" href="<?php bloginfo('url'); ?>/novedades#novedades">novedades</a></li>
+                     <li><a id="btn_tiendas" href="<?php bloginfo('url'); ?>/tiendas#tiendas">tiendas</a></li>
                    </ul>
                     <div class="divisor_h text-center"></div>
                      <ul class="nav navbar-nav lista_menu">
                      <li><a href="arauco-tag.php">arauco tag</a></li>
+                     <li><a href="<?php bloginfo('url'); ?>/arauco-tag">arauco tag</a></li>
                      <li><a href="http://www.revistadetalle.cl/" target="_blank">revista detalle</a></li>
-                     <li><a href="servicio-al-cliente.php">servicio al cliente</a></li>
-                     <li><a href="travellers.php">travellers</a></li>
+                     <li><a href="<?php bloginfo('url'); ?>/servicio-al-cliente">servicio al cliente</a></li>
+                     <li><a href="<?php bloginfo('url'); ?>/travellers">travellers</a></li>
                    </ul>
                    
                    <div class="divisor_h text-center"></div>
@@ -56,11 +57,11 @@
                               <div class="horarios">
                                     <ul class="clearfix">
                                       <li>
-                                            <a href="horarios.php"><i class="fa fa-clock-o" aria-hidden="true"></i>  horarios </a>
+                                        <a href="<?php bloginfo('url'); ?>/horarios.php"><i class="fa fa-clock-o" aria-hidden="true"></i>  horarios </a>
                                       </li>
 
                                       <li>
-                                            <a href="https://www.google.es/maps/place/Parque+Arauco/@-33.4019089,-70.5798102,18z/data=!4m13!1m7!3m6!1s0x9662cf4657be4221:0xedf29683ef50290a!2sAv+Presidente+Kennedy,+Regi%C3%B3n+Metropolitana,+Chile!3b1!8m2!3d-33.3977769!4d-70.5709762!3m4!1s0x9662cf26243dcc31:0xc9aacb64a9fec91!8m2!3d-33.4018773!4d-70.5792048" target="_blank"><i class="fa fa-map-marker" aria-hidden="true"></i>cómo llegar </a>
+                                        <a href="https://www.google.es/maps/place/Parque+Arauco/@-33.4019089,-70.5798102,18z/data=!4m13!1m7!3m6!1s0x9662cf4657be4221:0xedf29683ef50290a!2sAv+Presidente+Kennedy,+Regi%C3%B3n+Metropolitana,+Chile!3b1!8m2!3d-33.3977769!4d-70.5709762!3m4!1s0x9662cf26243dcc31:0xc9aacb64a9fec91!8m2!3d-33.4018773!4d-70.5792048" target="_blank"><i class="fa fa-map-marker" aria-hidden="true"></i>cómo llegar </a>
                                       </li>
                                     </ul>
                               </div>
@@ -69,7 +70,7 @@
                         </div> <!-- row -->
                         <div class="row">
                           <div class="col-sm-12 col_box_news_letter"> 
-                            <div class="box_newsletter" style="background: url(assets/img/bg_box_newsletter.jpg);">  
+                            <div class="box_newsletter" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/bg_box_newsletter.jpg);">  
                               <h3>suscríbete</h3>
                               <form class="form_neswsletter" role="" id="form_neswsletter">
                                 <div class="box_input">
@@ -89,7 +90,7 @@
                         <div class="clearfix"></div>
                         <!-- mobile menu -->
                         <section class="search_home_xs visible-xs visible-sm">
-                        <?php include('include-search-home-mobile.php'); ?>
+                            <?php include('include-search-home-mobile.php'); ?>
                         </section>
                         </div>
                       </div><!-- col -->
