7ab1bafd0a3f7eac15e4c1a6a4b5c78a591ef7cb
Seo2 <seo2@seo2.cl>
2017-07-19 16:02:34 -0400

    se implementan los videos

--- novedades-galeria.php
+++ novedades-galeria.php
@@ -57,18 +57,27 @@ if(ICL_LANGUAGE_CODE=='en'){
 					}
 				?>
 	          </div>
-			  <?php if(2==1){ ?>
 	            <section class="galeria_videos ">
 	              <h4 class="divider">videos</h4>
 	                <div class="container">
 	                  <div class="row">
-	                      <div class="col-sm-6">
-	                        <div id="video2" class="box_video" data-video="_YZyLJecku4">
+							<?php	                                         
+							    $args = array(
+									'post_type' 		=> array('video'),
+									'posts_per_page' 	=> 2
+							    );
+								$the_query = new WP_Query ($args);
+							    $i = 0;
+							    if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
+							    	$i++;
+							?>	   
+	                    	<div class="col-sm-6">
+	                        <div id="video<?php echo $i; ?>" class="box_video" data-video="<?php echo get('id_video_youtube'); ?>">
 	                            <div class="lente_video"></div>
 	                            <!-- 560 x 300 -->
-	                            <div class="cap_video" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/day_off.png);"></div>
+	                            <div class="cap_video" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>);"></div>
 	                            <div class="yt-video embed-responsive embed-responsive-16by9">
-	                              <iframe  class="videito video2" width="420" height="315" src="" frameborder="0" allowfullscreen></iframe> 
+	                              <iframe  class="videito video<?php echo $i; ?>" width="420" height="315" src="" frameborder="0" allowfullscreen></iframe> 
 	                            </div>                        
 	                                <div class="btn_play_video">
 	                                  <a class="hvr-float play_video">
@@ -78,36 +87,20 @@ if(ICL_LANGUAGE_CODE=='en'){
 	                            </div>
 	                        </div>
 	                      </div>
-	                       <div class="col-sm-6">
-	                        <div id="video1" class="box_video" data-video="bkPN-msE7SI">
-	                            <div class="lente_video"></div>
-	                              <!-- 560x300-->
-	                            <div class="cap_video" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/navidad.jpg);"> </div>
-	                            <div class="yt-video embed-responsive embed-responsive-16by9">
-	                               <iframe  class="videito video1"  width="420" height="315" src="" frameborder="0" allowfullscreen></iframe> 
-	                            </div>
-	                            <div class="btn_play_video">
-	                                  <a class="hvr-float play_video">
-	                                      <img src="<?php bloginfo('template_url'); ?>/assets/img/btn_play_video.png" alt="">
-	                                  </a>
-	                                  <p>ver video</p>                                 
-	                            </div>
-	                        </div>
-	                      </div>
+							<?php endwhile; else: ?>
+							<?php endif; ?>
 	                      
 	
 	                  </div>
 	                </div> <!-- container-videos -->
 	
 	            </section>
-	            
 	            <div class="box_ver_mas_tiendas text-center">
-	             <a href="https://www.youtube.com/user/parquearauco/videos" target="_blank" class="btn btn-default btn_ver_mas hvr-float">
+	             <a href="https://www.youtube.com/channel/UC9ZtYWgO0_Fz7Fm7A3loI8A" target="_blank" class="btn btn-default btn_ver_mas hvr-float">
 	                <h3 class="ver_mas">ver más videos</h3>
 	                <img src="<?php bloginfo('template_url'); ?>/assets/img/arrow_down.png" alt="" class="img-responsive center-block arrow_down">
 	            </a>
 	          </div>
-	          <?php } ?>
 	        </section> <!-- entretencion -->
 	      </div><!-- row -->
 
@@ -147,7 +140,7 @@ if(ICL_LANGUAGE_CODE=='en'){
 
 <script>
 <?php if($paginas<=1){ ?>
-	$('.box_ver_mas_tiendas').hide();
+	$('.box_ver_mas_tiendas2').hide();
 <?php } ?>	
 	
 	
@@ -162,7 +155,7 @@ $('#pages a').each(function(index) {
 
 
 
-$('.box_ver_mas_tiendas').on('click', function(){
+$('.box_ver_mas_tiendas2').on('click', function(){
   	loaded[pages[current+1]] = loaded[pages[current+1]] + 1; 
     if(loaded[pages[current+1]] <= 1){
          loadMoreContent(current+1);
@@ -181,7 +174,7 @@ function loadMoreContent(position) {
 			        if(position +1 < pages.length) {
 					
 					}else{
-					 	$('.box_ver_mas_tiendas').hide();
+					 	$('.box_ver_mas_tiendas2').hide();
 					}
 			    });
 	    	});
