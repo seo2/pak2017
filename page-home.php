736e0266f6514554f0d811ecc60fa2823ec3d609
Seo2 <seo2@seo2.cl>
2017-08-01 16:44:32 -0400

    Cambio de destacado del home. Revista detalle ahora es slider administrable.

--- page-home.php
+++ page-home.php
@@ -214,16 +214,22 @@ if(ICL_LANGUAGE_CODE=='en'){
                    <!--  <div class="lente"></div> -->
                    <!-- SLIDER DESTACADO HOME -->
                    <div class="slider_destacado_home owl-carousel owl-theme box">
+					   	<?php
+						   	$e = 0;
+							$sliders = get_order_group('destacado_grande_imagen');
+							foreach($sliders as $slider){  
+								$e++; 
+						?>    	
                       <div class="item">
                          <!--  <div class="image_dynamic_portada" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/tendencias.gif);"> </div> -->
-                          <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/assets/img/tendencias.gif" alt="">
-                            <a href="" class="btn btn-default btn_rojo2"  role="button">ver más</a>
+                          <img class="img-responsive" src="<<?php echo get('destacado_grande_imagen',$slider); ?>" alt="">
+                          <?php 
+	                          if(get('destacado_grande_link',$slider)){
+                          ?>
+                            <a href="<?php echo get('destacado_grande_link',$slider); ?>" class="btn btn-default btn_rojo2"  role="button">ver más</a>
+                          <?php } ?>
                       </div>
-                      <div class="item">
-                         <!--  <div class="image_dynamic_portada" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/revista_detalle.jpg);"> </div> -->
-                         <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/assets/img/revista_detalle.jpg" alt="">
-                           <a href="" class="btn btn-default btn_rojo2"  role="button">ver más</a>
-                      </div>
+                        <?php } ?>
                    </div>
                    <!--  <div class="caption_revista" >
                       <div class="logo_revista" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/logo_revista_detalle.svg);"> </div>
